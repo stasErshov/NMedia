ce40415a1d76b72b4936b436a4589a6f9e233237
Стас <popkin.popich@bk.ru>
2022-09-29 22:21:42 +0300

    Initall commit

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,11 +1,9 @@
 package ru.netology.nmedia.dto
 
-data class Post (
+data class Post(
     val id: Long,
     val author: String,
     val content: String,
     val published: String,
-    var likes: Int = 0,
-    var shareCountSum: Int = 0,
-    var likedByMe: Boolean = false
+    val likedByMe: Boolean,
 )
